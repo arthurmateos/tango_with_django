d484d27c790b6ebe67a7d81ecba00e64a5d67e3c
arthurmateos <artc97@gmail.com>
2016-07-22 15:33:27 -0400

    Finish 7.3.2

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
 
@@ -6,6 +8,11 @@ class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
+	slug = models.SlugField()
+	
+	def save(self):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 	
 	def __unicode__(self):
 		return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-	context_dict = {'boldmessage': "I am bold font from the context"}
+	context_list = Category.objects.order_by('-likes')
+	context_dict = {'categories': context_list}
+	
 	return render(request, 'rango/index.html', context_dict)
 	# return HttpResponse("Hello, world")
 
 def about(request):
 	return render(request, 'rango/about.html')
 	
+def category(request, category_name_slug):
+	context_dict = {}
+	
+	try:
